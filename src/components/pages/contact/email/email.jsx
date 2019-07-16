a546636900f15745ba458fa7de1a296efbe311bf
Jared Cox <coxj1@mcmaster.ca>
2019-07-15 20:34:51 -0400

    contact stuff

--- src/components/pages/contact/email/email.jsx
+++ src/components/pages/contact/email/email.jsx
@@ -5,32 +5,32 @@ export function email (props) {
 
         return (
             <div className="form-full">
-            <div className="form-container">
-                <h1 className="name-header">Name<span className="dot"></span></h1>
-                <input
-                type="text"
-                value={props.value}
-                className="__name-input"
-                placeholder = "Name"
-                onChange={props.handleNameChange}
-                />
-                <h1 className="email-header">Email<span className="dot"></span></h1>
-                <input
-                type="text"
-                value={props.value}
-                className="__email-input"
-                placeholder = "Email Address"
-                onChange={props.handleEmailChange}
-                />
-                <h1 className="message-header">Message<span className="dot"></span></h1>
-                <input
-                type="text"
-                value={props.value}
-                className="__message-input"
-                placeholder = "Message"
-                onChange={props.handleMessageChange}
-                />
-            </div>
+                <div className="form-container">
+                    <h1 className="name-header">Name<span className="dot"></span></h1>
+                    <input
+                    type="text"
+                    value={props.value}
+                    className="__name-input"
+                    placeholder = "Name"
+                    onChange={props.handleNameChange}
+                    />
+                    <h1 className="email-header">Email<span className="dot"></span></h1>
+                    <input
+                    type="text"
+                    value={props.value}
+                    className="__email-input"
+                    placeholder = "Email Address"
+                    onChange={props.handleEmailChange}
+                    />
+                    <h1 className="message-header">Message<span className="dot"></span></h1>
+                    <input
+                    type="text"
+                    value={props.value}
+                    className="__message-input"
+                    placeholder = "Message"
+                    onChange={props.handleMessageChange}
+                    />
+                </div>
             </div>
         )
     
