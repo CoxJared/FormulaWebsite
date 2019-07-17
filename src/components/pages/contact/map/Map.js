fe59330ee4c1e41bae223d52b39274fb3b80b41d
Jared Cox <coxj1@mcmaster.ca>
2019-07-17 14:48:38 -0400

    mobile tweaks

--- src/components/pages/contact/map/Map.js
+++ src/components/pages/contact/map/Map.js
@@ -5,7 +5,7 @@ export class Map extends Component {
     render() {
         return (
             <div className="map-container">
-                <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=McMaster%20University%20Hamilton%2C%20John%20Hodgins%20Engineering&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-vpn/">mac</a></div></div>
+                <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=McMaster%20University%20Hamilton%2C%20John%20Hodgins%20Engineering&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-vpn/">mac</a></div></div>
                 <div className="address-text-box">
                     <h1 className="address-header">
                         Address
