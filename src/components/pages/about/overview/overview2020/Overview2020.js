15c1c5de84840c2e08a68f2bd2cd2cb51591bc25
Jared Cox <coxj1@mcmaster.ca>
2019-07-30 21:00:39 -0400

    team fix

--- src/components/pages/about/overview/overview2020/Overview2020.js
+++ src/components/pages/about/overview/overview2020/Overview2020.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './overview2020.css';
 
-export class Overview2020 extends Component {
-    render() {
-        return (
-            <div className="overiew-2020-container">
-                {/* <div className="overview-2020-intro-title-container">
-                        <h1 className="overview-2020-intro-title">This Season</h1>
-                </div> */}
-                <p className="overview-2020-paragraph">Season starts in September. Stay tuned!</p>
-            </div>
-        )
-    }
+export function Overview2020 () {
+    return (
+        <div className="overiew-2020-container">
+            <p className="overview-2020-paragraph">Season starts in September. Stay tuned!</p>
+        </div>
+    )
 }
 
 export default Overview2020
--- src/components/pages/about/team/TeamMember.js
+++ src/components/pages/about/team/TeamMember.js
@@ -1,55 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './TeamMember.css';
 
-export function TeamMember () {
-    return (
-        <div>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-            <div className="member-popup-container-full-screem" onClick={this.props.closeMember} />
-            <div className="member-popup-container">
-                <div className="member-popup-left-container">
-                    <div className="member-popup-left-image-container">
-                        <img src={this.props.member.headshot} className="member-popup-left-image"/>
-                    </div>
-                    <div className="member-popup-left-email-container">
-                        <h1 className="member-popup-left-email">{this.props.member.email}</h1>
-                    </div>
-                    <div className="member-popup-left-links-container">
-                        <div className="member-poup-left-links">            
-                                {this.props.member.links.map(link => ( 
-                            <a href={link.url} class={`fa fa-${link.name} custom-${link.name} smaller`} target="_blank" rel="noopener noreferrer" />
-                            ))}
+export class TeamMember extends Component {
+    render(){
+        return (
+            <div>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                <div className="member-popup-container-full-screem" onClick={this.props.closeMember} />
+                <div className="member-popup-container">
+                    <div className="member-popup-left-container">
+                        <div className="member-popup-left-image-container">
+                            <img src={this.props.member.headshot} className="member-popup-left-image"/>
                         </div>
-                    </div>      
-                </div>
-                <div className="member-popup-right-container">
-                    <div className="member-popup-right-name-container">
-                        <h1 className="member-popup-right-name">{this.props.member.name}</h1>
-                    </div>
-                    <div className="member-popup-right-title-container">
-                        <h1 className="member-popup-right-title">{this.props.member.seasons[this.props.season].title}</h1>
-                    </div>
-                    <div className="member-popup-right-experience-container">
-
-                        {this.props.member.projects.map(projectYear => (
-                            <div>
-                                <h1 className="member-popup-right-experience-title">{projectYear.year} projects</h1>
-                                {projectYear.projectsForYear.map(project => (
-                                    <>
-                                        <h2 className="member-popup-right-experience-item">- {project}</h2>
-                                    </>
+                        <div className="member-popup-left-email-container">
+                            <h1 className="member-popup-left-email">{this.props.member.email}</h1>
+                        </div>
+                        <div className="member-popup-left-links-container">
+                            <div className="member-poup-left-links">            
+                                    {this.props.member.links.map(link => ( 
+                                <a href={link.url} class={`fa fa-${link.name} custom-${link.name} smaller`} target="_blank" rel="noopener noreferrer" />
                                 ))}
                             </div>
-                        ))}
+                        </div>      
                     </div>
+                    <div className="member-popup-right-container">
+                        <div className="member-popup-right-name-container">
+                            <h1 className="member-popup-right-name">{this.props.member.name}</h1>
+                        </div>
+                        <div className="member-popup-right-title-container">
+                            <h1 className="member-popup-right-title">{this.props.member.seasons[this.props.season].title}</h1>
+                        </div>
+                        <div className="member-popup-right-experience-container">
+
+                            {this.props.member.projects.map(projectYear => (
+                                <div>
+                                    <h1 className="member-popup-right-experience-title">{projectYear.year} projects</h1>
+                                    {projectYear.projectsForYear.map(project => (
+                                        <>
+                                            <h2 className="member-popup-right-experience-item">- {project}</h2>
+                                        </>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <button className="member-popup-close-button" onClick={this.props.closeMember}>
+                        <div className="member-popup-close-button-line1" />
+                        <div className="member-popup-close-button-line2" />
+                    </button>
                 </div>
-                <button className="member-popup-close-button" onClick={this.props.closeMember}>
-                    <div className="member-popup-close-button-line1" />
-                    <div className="member-popup-close-button-line2" />
-                </button>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default TeamMember
