659c23f1069302cf89c45702cdc4e01a15e691a2
Jared Cox <coxj1@mcmaster.ca>
2019-08-02 10:21:10 -0400

    update 2018 overview

--- src/components/pages/about/overview/overview2018/Overview2018.js
+++ src/components/pages/about/overview/overview2018/Overview2018.js
@@ -3,8 +3,13 @@ import './Overview2018.css';
 
 export function Overview2018 () {
     return (
-        <div className="overview2018-container">
-            hi
+        <div className="overview2019-full-container">
+        <div className="overview2018-showcase">
+            
+        </div>
+        <p className="overview2019-intro-para">
+        MAC Formula Electric began as a small group of dedicated students who have grown the team into what it is now, a team of over 80 active members working on various aspects of the car.  The team spans over all engineering faculties as well as applied sciences, creating a well rounded and functional group dynamic. As the group of original founding members graduated this past year, our team has embraced the opportunity to re-structure and accept new, ambitious young engineering students who will build upon what has been achieved so far. The team is currently completing the build phase of our fully electric, all wheel drive, Formula 1 style racecar.  Here are some of the milestones we have achieved this year.
+                </p>
         </div>
     )
 }
