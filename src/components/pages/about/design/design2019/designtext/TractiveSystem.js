743c69edeb057eac3138ea0fb8afeda4e01f8cd3
Jared Cox <coxj1@mcmaster.ca>
2019-08-02 18:03:04 -0400

    fix team member 2018

--- src/components/pages/about/design/design2019/designtext/TractiveSystem.js
+++ src/components/pages/about/design/design2019/designtext/TractiveSystem.js
@@ -7,7 +7,7 @@ export function TractiveSystem () {
                 Tractive System
             </h1>
             <p className="general-paragraph">
-            This year, we developed our first high-voltage battery with three priorities: high specific-power, high specific-energy, and light-weight. This design will be capable of providing the maximum power to the vehicle for the entirety of the endurance run. With a charge time of approximately 35 minutes, we are able to reduce the downtime between runs and improve our teams overall efficiency. The next step we are taking to solidify this design is to subject the accumulator to some rigorous cycle testing in our battery testing lab. This will allow us to validate our simulations and gather data to improve on the next design. Finally, after we have confirmed the functionality of this design, we will be taking it to the track for some additional testing and tuning.
+            This year, we developed our first high-voltage battery with three priorities: high specific-power, high specific-energy, and light-weight. This design will be capable of providing the maximum power to the vehicle for the entirety of the endurance run. With a charge time of approximately 35 minutes, we are able to reduce the downtime between runs and improve our teams overall efficiency. 
             </p>
         </div>
     )
