3e137bcb04397865d0a8a93780645ad5d832ba28
Jared Cox <coxj1@mcmaster.ca>
2019-08-11 11:15:45 -0400

    header

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -20,49 +20,33 @@ class Header extends Component{
     
 
     render() {
-        var prevScrollpos = window.pageYOffset;
-        window.onscroll = function() {
-            var currentScrollPos = window.pageYOffset;
-
-            if(currentScrollPos > 50)
-            {
-                (document.getElementById("header")).style.backgroundColor="#fff";
-                (document.getElementById("header")).style.boxShadow="3px 3px 10px grey";
-                (document.getElementById("header")).style.color="#333";
+        var headerClass = "static-header";
+        if(this.props.page !== "contact" && this.props.page !== "sponsors"){
+            headerClass = "header"
+            window.onscroll = function() {
+                try{
+                var currentScrollPos = window.pageYOffset;
+                if(currentScrollPos > 50)
+                {
+                    (document.getElementById("header")).style.backgroundColor="#fff";
+                    (document.getElementById("header")).style.boxShadow="3px 3px 10px grey";
+                    (document.getElementById("header")).style.color="#333";
+                }
+                else{
+                    (document.getElementById("header")).style.background="none";
+                    (document.getElementById("header")).style.boxShadow="none";
+                    (document.getElementById("header")).style.color="#fff";
+                }
+            }catch{}
             }
-            else{
-                (document.getElementById("header")).style.background="none";
-                (document.getElementById("header")).style.boxShadow="none";
-                (document.getElementById("header")).style.color="#fff";
-            }
-
-
-            // if (prevScrollpos < (currentScrollPos +1) ) {
-            //     document.getElementById("toggle").checked = false;
-            //     if(currentScrollPos > 90)
-            //     {
-            //         (document.getElementById("header-container")).style.top="-90px";
-            //     }
-            //     else{
-            //         (document.getElementById("header-container")).style.top="-".concat(currentScrollPos,"px");
-            //     }
-            // } else {
-            //     (document.getElementById("header-container")).style.top="0" ;
-            // }
-            prevScrollpos = currentScrollPos;
         }
-        // if(this.props.page === "contact" || this.props.page === "sponsors")
-        // {
-        //     (document.getElementById("header")).style.backgroundColor="#fff";
-        //     (document.getElementById("header")).style.boxShadow="3px 3px 10px grey";
-        //     (document.getElementById("header")).style.color="#333";
-        // }
+
         return (
             <div>
                 <link href="https://fonts.googleapis.com/css?family=Rajdhani:520,600|Ubuntu&display=swap" rel="stylesheet"/>
             <div id="header-container">
             <div className="tracker-backgound">
-                <div id="header">
+                <div id={`${headerClass}`}>
                     <div className="logo">
                     <Link className="logo-image" to="/">
                         <img src={logoImage} className="logo-image" alt=""></img> 
