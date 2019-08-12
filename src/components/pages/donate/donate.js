d9cdbb26fa4e784d660c684ee69f64255219b2d2
Jared Cox <coxj1@mcmaster.ca>
2019-08-11 21:01:23 -0400

    capitalize

--- src/components/pages/donate/donate.js
+++ src/components/pages/donate/donate.js
@@ -11,7 +11,7 @@ export class donate extends Component {
                 <div className="why-donate-container-page">
 
                     <div className="why-donate-showcase">
-                        <h1 className="why-donate-header">Support the engineers of tomorrow</h1>
+                        <h1 className="why-donate-header">SUPPORT THE ENGINEERS OF TOMORROW</h1>
                     </div>
                 <div className="why-donate-paragraph-container">
                     <p className="why-donate-paragraph">With the ambitious goals the team has set, the use of advanced materials, manufacturing techniques, and components are vital to success. Due to the nature of the relatively new electric vehicle industry, many of the technologies being implemented into the car are also new. In general, this means low volume at a high cost. By donating to the team, you will be providing us with the resources necessary to turn our designs into reality.
