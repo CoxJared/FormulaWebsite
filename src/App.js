9b4714d7529653e70e9ed42e05bd8330dc5a57be
Jared Cox <coxj1@mcmaster.ca>
2019-08-26 17:31:20 -0400

    add controls postings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { BrowserRouter as Router,  Route } from 'react-router-dom';
 
@@ -9,27 +9,30 @@ import About from './components/pages/about/About';
 import Recruitment from './components/pages/recruitement/Recruitment';
 import donate from './components/pages/donate/donate';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+    return (
 
-    <Router>
-      <link href="https://fonts.googleapis.com/css?family=Rajdhani:520,600|Ubuntu&display=swap" rel="stylesheet"/>
-      <div className="app">
-          <Route exact path="/" 
-              component={Home} />
-          <Route path="/about" 
-              component={About} /> 
-          <Route path="/recruitment" 
-              component={Recruitment} />
-          <Route path="/sponsors" 
-              component={Sponsors} />
-          <Route path="/contact" 
-              component={Contact} />
-          <Route path="/donate" 
-              component={donate} />
-      </div>
-    </Router>
-  )
+      <Router>
+        <link href="https://fonts.googleapis.com/css?family=Rajdhani:520,600|Ubuntu&display=swap" rel="stylesheet"/>
+        <div className="app">
+            <Route exact path="/" 
+                component={Home} />
+            <Route path="/about" 
+                component={About} /> 
+            <Route path="/recruitment" 
+                component={Recruitment} />
+            <Route path="/sponsors" 
+                component={Sponsors} />
+            <Route path="/contact" 
+                component={Contact} />
+            <Route path="/donate" 
+                component={donate} />
+            <Route path='recruitment/:handle' component={donate} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
--- src/components/pages/recruitement/positions/position/Position.js
+++ src/components/pages/recruitement/positions/position/Position.js
@@ -34,11 +34,10 @@ export class Position extends Component {
                     <div className="position-posting-applynow-container">
 
                         {this.props.position.applyLink === '#'?
-                        <a className="position-posting-applynowi-disabled" href="#">Applications Open September 3rd</a>
-                        :<a className="position-posting-applynow" onClick={this.openApplication}>Apply Now</a>
+                            <a className="position-posting-applynowi-disabled" href="">Applications Open September 3rd</a>
+                            :<a className="position-posting-applynow"  onClick={this.openApplication}>Apply Now</a>
                         }
 
-                        
                     </div>
                     <h2 className="position-posting-date">Closing: <span style={{color:'#c22'}}>{this.props.position.openings}</span> </h2>
 
