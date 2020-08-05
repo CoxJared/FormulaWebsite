a895644859cd13f3b48035b87f1a3ea908b614e8
Jared Cox <coxj1@mcmaster.ca>
2020-08-04 22:17:56 -0400

    update pack

---
+++ src/components/loadingspinner/LoadingSpinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LoadingSpinner.scss';
+
+export function LoadingSpinner() {
+  return <div className="loading-spinner"></div>;
+}
+
+export default LoadingSpinner;
